4ee379bb54b199eec89033adcce07a563d37b3f1
Trung <trunglh113@gmail.com>
2025-10-14 01:49:25 +0700

    add

---
+++ zascapay/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+# ===============================
+# 1️⃣  Cài đặt gói hệ thống cần thiết
+# ===============================
+echo "📦 Installing system packages..."
+sudo apt update -y
+sudo apt install -y pkg-config libmysqlclient-dev build-essential gunicorn
+
+# ===============================
+# 2️⃣  Cài Python dependencies
+# ===============================
+echo "🐍 Installing Python dependencies..."
+if [ -f requirements.txt ]; then
+    pip install -r requirements.txt
+else
+    echo "⚠️  requirements.txt not found!"
+fi
+
+
+# ===============================
+# 3️⃣  Apply migrations
+# ===============================
+echo "🗃️ Applying Django migrations..."
+cd "$(pwd)/zascapay"
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+#echo "🔥 Starting  server..."
+#python runserver.py
